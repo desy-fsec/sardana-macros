52bcd22a4c10dadf0be912a5455d1ccb086412e6
zreszela <zreszela@users.sourceforge.net>
2013-02-25 14:13:51 +0000

    simplified macros; added macros for easier switching from cont to step scans
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/macros/trunk@5512 a256e3e2-f7d5-44d2-9cb2-a5f5fb33e127

--- ALBA_BL04_MSPD/mad_acq.py
+++ ALBA_BL04_MSPD/mad_acq.py
@@ -1,54 +1,54 @@
 import time
-import taurus
+import PyTango, taurus
 from sardana.macroserver.macro import *
 
-DEV2_CHANNELS = ["bl04/io/ibl0403-dev2-ctr0",
-                 "bl04/io/ibl0403-dev2-ctr1",
-                 "bl04/io/ibl0403-dev2-ctr2", 
-                 "bl04/io/ibl0403-dev2-ctr3",
-                 "bl04/io/ibl0403-dev2-ctr4",
-                 "bl04/io/ibl0403-dev2-ctr5",
-                 "bl04/io/ibl0403-dev2-ctr6",
-                 "bl04/io/ibl0403-dev2-ctr7"]
+COUNTERS = ["bl04/io/ibl0403-dev2-ctr0",
+            "bl04/io/ibl0403-dev2-ctr1",
+            "bl04/io/ibl0403-dev2-ctr2", 
+            "bl04/io/ibl0403-dev2-ctr3",
+            "bl04/io/ibl0403-dev2-ctr4",
+            "bl04/io/ibl0403-dev2-ctr5",
+            "bl04/io/ibl0403-dev2-ctr6",
+            "bl04/io/ibl0403-dev2-ctr7",
+            "bl04/io/ibl0403-dev3-ctr0",
+            "bl04/io/ibl0403-dev3-ctr1",
+            "bl04/io/ibl0403-dev3-ctr2",
+            "bl04/io/ibl0403-dev3-ctr3",
+            "bl04/io/ibl0403-dev3-ctr4",
+            "bl04/io/ibl0403-dev3-ctr5"]
 
-DEV3_CHANNELS = ["bl04/io/ibl0403-dev3-ctr0",
-                 "bl04/io/ibl0403-dev3-ctr1",
-                 "bl04/io/ibl0403-dev3-ctr2",
-                 "bl04/io/ibl0403-dev3-ctr3",
-                 "bl04/io/ibl0403-dev3-ctr4",
-                 "bl04/io/ibl0403-dev3-ctr5"]
-                 
-MAD_MNT_GRP = "mad"
+TRIGGER = "bl04/io/ibl0403-dev3-ctr6"
+TRIGGER_DEVICE = "bl04/io/ibl0403-dev3"
+TRIGGER_SOURCE = "/Dev3/PFI12"
+TRIGGER_DESTINATION = "/Dev3/RTSI0"
+
+MNT_GRP = "mad"
 
 class PrepareGating(Macro):
 
     param_def = []
 
     def run(self, *args, **kwargs):
-        dev = taurus.Device("bl04/io/ibl0403-dev3")
-        dev.ConnectTerms(["/Dev3/PFI12", "/Dev3/RTSI0", "DoNotInvertPolarity"])
+        dev = taurus.Device(TRIGGER_DEVICE)
+        dev.ConnectTerms([TRIGGER_SOURCE, TRIGGER_DESTINATION, "DoNotInvertPolarity"])
 
 class CleanupGating(Macro):
 
     param_def = []
 
     def run(self, *args, **kwargs):
-        dev = taurus.Device("bl04/io/ibl0403-dev3")
-        dev.DisconnectTerms(["/Dev3/PFI12", "/Dev1/RTSI0"])
+        dev = taurus.Device(TRIGGER_DEVICE)
+        dev.DisconnectTerms([TRIGGER_SOURCE, TRIGGER_DESTINATION])
 
 class PrepareMaster(Macro):
 
     param_def = []
 
     def run(self, *args, **kwargs):
-        activeMntGrpName = self.getEnv("ActiveMntGrp")
-        mntGrp = taurus.Device(activeMntGrpName)
-        timerName = mntGrp.getAttribute("ElementList").read().value[0]
-        self.debug(timerName)
-        timer = taurus.Device(timerName)
-        timerChannelName = timer.getAttribute("channelDevName").read().value
-        timerChannel = taurus.Device(timerChannelName)
-        timerChannel.getAttribute("InitialDelayTime").write(4)
+        timerChannel = taurus.Device(TRIGGER)
+        if timerChannel.State() != PyTango.DevState.STANDBY:
+            timerChannel.Stop()
+        timerChannel.getAttribute("InitialDelayTime").write(0)
         timerChannel.getAttribute("SampleMode").write("Finite")
         timerChannel.getAttribute("SampleTimingType").write("Implicit")
         timerChannel.getHWObj().write_attribute("SampPerChan", long(1))
@@ -60,69 +60,15 @@ class PrepareSlaves(Macro):
     param_def = []
 
     def run(self, *args, **kwargs):
-        #activeMntGrpName = self.getEnv("ActiveMntGrp")
-        #mntGrp = taurus.Device(activeMntGrpName)
-        #counterNames = mntGrp.getAttribute("ElementList").read().value[1:]
-        #self.debug(counterNames)
-        
-        #counters = []
-        #for counterName in counterNames:
-            #self.debug("counterName: %s" % counterName)
-            #counters.append(taurus.Device(counterName))
-        #self.debug(len(counters))
-        #channels = []
-        #for counter in counters:
-            #channelName = counter.getAttribute("channelDevName").read().value
-            #self.debug("channelName: %s" % channelName)
-            #channels.append(taurus.Device(channelName))
-        channels = []
-        for channelName in DEV2_CHANNELS:
-            channels.append(taurus.Device(channelName))
-
-        for channel in channels:
-            self.debug(repr(channel))
+        for channelName in COUNTERS:
+            channel = taurus.Device(channelName)
+            if channel.State() != PyTango.DevState.STANDBY:
+                channel.Stop()
             channel.getAttribute("PauseTriggerType").write("DigLvl")
             channel.getAttribute("PauseTriggerWhen").write("Low")
-            channel.getAttribute("PauseTriggerSource").write("/Dev2/RTSI0")
+            channel.getAttribute("PauseTriggerSource").write(TRIGGER_DESTINATION)
 
-        channels = []
-        for channelName in DEV3_CHANNELS:
-            channels.append(taurus.Device(channelName))
-
-        for channel in channels:
-            self.debug(repr(channel))
-            channel.getAttribute("PauseTriggerType").write("DigLvl")
-            channel.getAttribute("PauseTriggerWhen").write("Low")
-            channel.getAttribute("PauseTriggerSource").write("/Dev3/RTSI0")
-
-class StartTestCount(Macro):
-
-    param_def = []
-
-    def run(self, *args, **kwargs):
-        ch0 = taurus.Device("bl04/io/ibl0403-dev2-ctr0")
-        ch1 = taurus.Device("bl04/io/ibl0403-dev2-ctr1")
-        channels = [ch0, ch1]
-        for channel in channels:
-            channel.getAttribute("HighTicks").write(400000)
-            channel.getAttribute("LowTicks").write(400000)
-            channel.getAttribute("SampleMode").write("Cont")
-        for channel in channels:
-            channel.start()
-
-
-class StopTestCount(Macro):
-
-    param_def = []
-
-    def run(self, *args, **kwargs):
-        ch2 = taurus.Device("bl04/io/ibl0403-dev2-ctr0")
-        ch3 = taurus.Device("bl04/io/ibl0403-dev2-ctr1")
-        channels = [ch2, ch3]
-        for channel in channels:
-            channel.stop()
-
-class PrepareScan(Macro):
+class PrepareCountersForStepScanning(Macro):
 
     param_def = []
 
@@ -130,54 +76,15 @@ class PrepareScan(Macro):
         self.execMacro("PrepareMaster")
         self.execMacro("PrepareSlaves")
         self.execMacro("PrepareGating")
-
-class InitCounters(Macro):
-
-    param_def = []
-
-    def run(self, *args, **kwargs):
-        ni660x_timer = taurus.Device("it")
-        ni660x_timer.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr6")
-        ni660x_ch0 = taurus.Device("i1")
-        ni660x_ch0.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr0")
-        ni660x_ch1 = taurus.Device("i2")
-        ni660x_ch1.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr1")
-        ni660x_ch2 = taurus.Device("i3")
-        ni660x_ch2.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr2")
-        ni660x_ch3 = taurus.Device("i4")
-        ni660x_ch3.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr3")
-        ni660x_ch4 = taurus.Device("i5")
-        ni660x_ch4.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr4")
-        ni660x_ch5 = taurus.Device("i6")
-        ni660x_ch5.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr5")
-        ni660x_ch6 = taurus.Device("i7")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr6")
-        ni660x_ch6 = taurus.Device("i8")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr7")
-        ni660x_ch6 = taurus.Device("i9")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr0")
-        ni660x_ch6 = taurus.Device("i10")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr1")
-        ni660x_ch6 = taurus.Device("i11")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr2")
-        ni660x_ch6 = taurus.Device("i12")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr3")
-        ni660x_ch6 = taurus.Device("i13")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr4")
-        ni660x_ch6 = taurus.Device("i14")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr5")
         
-
-class CreateCounters(Macro):
-
+class InitCounters(Macro):
+    
     param_def = []
-
+    
     def run(self, *args, **kwargs):
-        self.execMacro("defelem", "it", "mad26_ct_ctrl", 1)
-        time.sleep(1)
-        for i in range(14):
-            self.execMacro("defelem", "i%d" % i, "mad26_ct_ctrl", i+2)
-            time.sleep(1)
+        for channelName in COUNTERS:
+            channel = taurus.Device(channelName)
+            channel.Init()    
 
 class mad_ct(Macro):
     
@@ -185,19 +92,16 @@ class mad_ct(Macro):
 
     def run(self, *args, **kwargs):
         time = args[0]
-        mntGrp = self.getEnv("ActiveMntGrp")
-        self.execMacro("senv", "ActiveMntGrp", MAD_MNT_GRP)
-        self.execMacro("PrepareMaster")
-        self.execMacro("PrepareSlaves")
-        self.execMacro("PrepareGating")
+        oldMntGrp = self.getEnv("ActiveMntGrp")
+        self.setEnv("ActiveMntGrp", MNT_GRP)
+        self.execMacro("PrepareCountersForStepScanning")
         try:
             self.execMacro("ct", time)
         finally:
-            self.execMacro("CleanupGating")
-            self.execMacro("senv", "ActiveMntGrp", mntGrp)
+            self.setEnv("ActiveMntGrp", oldMntGrp)
 
 class mad_ascan(Macro):
-    """newfile [numor] [numor file] [ numor file directory]"""
+    
     param_def = [ ['motor',      Type.Moveable,  None, 'Moveable to move'],
               ['start_pos',  Type.Float,   None, 'Scan start position'],
               ['final_pos',  Type.Float,   None, 'Scan final position'],
@@ -212,13 +116,10 @@ class mad_ascan(Macro):
         Npts      = args[3]
         intTim    = args[4]      
         args = (mot.name,StartPos,EndPos,Npts,intTim)
-        mntGrp = self.getEnv("ActiveMntGrp")
-	self.execMacro("senv", "ActiveMntGrp", MAD_MNT_GRP)
-        self.execMacro("PrepareMaster")
-        self.execMacro("PrepareSlaves")
-        self.execMacro("PrepareGating")
+        oldMntGrp = self.getEnv("ActiveMntGrp")
+        self.setEnv("ActiveMntGrp", MNT_GRP)
+        self.execMacro("PrepareCountersForStepScanning")
         try:
             self.execMacro('ascan', *args)
         finally: 
-            self.execMacro('CleanupGating') 
             self.execMacro("senv", "ActiveMntGrp", mntGrp)
--- ALBA_BL04_MSPD/mad_acq_lab.py
+++ ALBA_BL04_MSPD/mad_acq_lab.py
@@ -2,8 +2,6 @@ import time
 import PyTango, taurus
 from sardana.macroserver.macro import *
 
-DEVICE = "zreszela/io/ictlael01-dev1"
-
 COUNTERS = ["zreszela/io/ictlael01-dev1-ch1",
             "zreszela/io/ictlael01-dev1-ch2",
             "zreszela/io/ictlael01-dev1-ch3",
@@ -12,6 +10,9 @@ COUNTERS = ["zreszela/io/ictlael01-dev1-ch1",
            ]
 
 TRIGGER = "zreszela/io/ictlael01-dev1-ch6"
+TRIGGER_DEVICE = "zreszela/io/ictlael01-dev1"
+TRIGGER_SOURCE = "/Dev1/PFI12"
+TRIGGER_DESTINATION = "/Dev1/RTSI0"
 
 MNT_GRP = "counters"
 
@@ -20,22 +21,22 @@ class PrepareGating(Macro):
     param_def = []
 
     def run(self, *args, **kwargs):
-        dev = taurus.Device(DEVICE)
-        dev.ConnectTerms(["/Dev1/PFI12", "/Dev1/RTSI0", "DoNotInvertPolarity"])
+        dev = taurus.Device(TRIGGER_DEVICE)
+        dev.ConnectTerms([TRIGGER_SOURCE, TRIGGER_DESTINATION, "DoNotInvertPolarity"])
 
 class CleanupGating(Macro):
 
     param_def = []
 
     def run(self, *args, **kwargs):
-        dev = taurus.Device(DEVICE)
-        dev.DisconnectTerms(["/Dev1/PFI12", "/Dev1/RTSI0"])
+        dev = taurus.Device(TRIGGER_DEVICE)
+        dev.DisconnectTerms([TRIGGER_SOURCE, TRIGGER_DESTINATION])
 
 class PrepareMaster(Macro):
 
     param_def = []
 
-    def run(self, *args, **kwargs):        
+    def run(self, *args, **kwargs):
         timerChannel = taurus.Device(TRIGGER)
         if timerChannel.State() != PyTango.DevState.STANDBY:
             timerChannel.Stop()
@@ -44,90 +45,22 @@ class PrepareMaster(Macro):
         timerChannel.getAttribute("SampleTimingType").write("Implicit")
         timerChannel.getHWObj().write_attribute("SampPerChan", long(1))
         timerChannel.getAttribute("IdleState").write("Low")        
-        timerChannel.getAttribute("LowTime").write(0.000000125)        
+        timerChannel.getAttribute("LowTime").write(0.0000001)        
 
 class PrepareSlaves(Macro):
     
     param_def = []
 
     def run(self, *args, **kwargs):
-        #activeMntGrpName = self.getEnv("ActiveMntGrp")
-        #mntGrp = taurus.Device(activeMntGrpName)
-        #counterNames = mntGrp.getAttribute("ElementList").read().value[1:]
-        #self.debug(counterNames)
-        
-        #counters = []
-        #for counterName in counterNames:
-            #self.debug("counterName: %s" % counterName)
-            #counters.append(taurus.Device(counterName))
-        #self.debug(len(counters))
-        #channels = []
-        #for counter in counters:
-            #channelName = counter.getAttribute("channelDevName").read().value
-            #self.debug("channelName: %s" % channelName)
-            #channels.append(taurus.Device(channelName))
         for channelName in COUNTERS:
             channel = taurus.Device(channelName)
             if channel.State() != PyTango.DevState.STANDBY:
                 channel.Stop()
             channel.getAttribute("PauseTriggerType").write("DigLvl")
             channel.getAttribute("PauseTriggerWhen").write("Low")
-            channel.getAttribute("PauseTriggerSource").write("/Dev1/RTSI0")
-            
-class CleanupSlaves(Macro):
-    
-    param_def = []
+            channel.getAttribute("PauseTriggerSource").write(TRIGGER_DESTINATION)
 
-    def run(self, *args, **kwargs):
-        #activeMntGrpName = self.getEnv("ActiveMntGrp")
-        #mntGrp = taurus.Device(activeMntGrpName)
-        #counterNames = mntGrp.getAttribute("ElementList").read().value[1:]
-        #self.debug(counterNames)
-        
-        #counters = []
-        #for counterName in counterNames:
-            #self.debug("counterName: %s" % counterName)
-            #counters.append(taurus.Device(counterName))
-        #self.debug(len(counters))
-        #channels = []
-        #for counter in counters:
-            #channelName = counter.getAttribute("channelDevName").read().value
-            #self.debug("channelName: %s" % channelName)
-            #channels.append(taurus.Device(channelName))
-        for channelName in COUNTERS:
-            channel = taurus.Device(channelName)
-            if channel.State() != PyTango.DevState.STANDBY:
-                channel.Stop()
-            channel.getAttribute("PauseTriggerType").write("None")        
-
-class StartTestCount(Macro):
-
-    param_def = []
-
-    def run(self, *args, **kwargs):
-        ch0 = taurus.Device("bl04/io/ibl0403-dev2-ctr0")
-        ch1 = taurus.Device("bl04/io/ibl0403-dev2-ctr1")
-        channels = [ch0, ch1]
-        for channel in channels:
-            channel.getAttribute("HighTicks").write(400000)
-            channel.getAttribute("LowTicks").write(400000)
-            channel.getAttribute("SampleMode").write("Cont")
-        for channel in channels:
-            channel.start()
-
-
-class StopTestCount(Macro):
-
-    param_def = []
-
-    def run(self, *args, **kwargs):
-        ch2 = taurus.Device("bl04/io/ibl0403-dev2-ctr0")
-        ch3 = taurus.Device("bl04/io/ibl0403-dev2-ctr1")
-        channels = [ch2, ch3]
-        for channel in channels:
-            channel.stop()
-
-class PrepareScan(Macro):
+class PrepareCountersForStepScanning(Macro):
 
     param_def = []
 
@@ -135,54 +68,15 @@ class PrepareScan(Macro):
         self.execMacro("PrepareMaster")
         self.execMacro("PrepareSlaves")
         self.execMacro("PrepareGating")
-
-class InitCounters(Macro):
-
-    param_def = []
-
-    def run(self, *args, **kwargs):
-        ni660x_timer = taurus.Device("it")
-        ni660x_timer.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr6")
-        ni660x_ch0 = taurus.Device("i1")
-        ni660x_ch0.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr0")
-        ni660x_ch1 = taurus.Device("i2")
-        ni660x_ch1.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr1")
-        ni660x_ch2 = taurus.Device("i3")
-        ni660x_ch2.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr2")
-        ni660x_ch3 = taurus.Device("i4")
-        ni660x_ch3.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr3")
-        ni660x_ch4 = taurus.Device("i5")
-        ni660x_ch4.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr4")
-        ni660x_ch5 = taurus.Device("i6")
-        ni660x_ch5.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr5")
-        ni660x_ch6 = taurus.Device("i7")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr6")
-        ni660x_ch6 = taurus.Device("i8")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev2-ctr7")
-        ni660x_ch6 = taurus.Device("i9")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr0")
-        ni660x_ch6 = taurus.Device("i10")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr1")
-        ni660x_ch6 = taurus.Device("i11")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr2")
-        ni660x_ch6 = taurus.Device("i12")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr3")
-        ni660x_ch6 = taurus.Device("i13")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr4")
-        ni660x_ch6 = taurus.Device("i14")
-        ni660x_ch6.getAttribute("channelDevName").write("bl04/io/ibl0403-dev3-ctr5")
         
-
-class CreateCounters(Macro):
-
+class InitCounters(Macro):
+    
     param_def = []
-
+    
     def run(self, *args, **kwargs):
-        self.execMacro("defelem", "it", "mad26_ct_ctrl", 1)
-        time.sleep(1)
-        for i in range(14):
-            self.execMacro("defelem", "i%d" % i, "mad26_ct_ctrl", i+2)
-            time.sleep(1)
+        for channelName in COUNTERS:
+            channel = taurus.Device(channelName)
+            channel.Init()    
 
 class mad_ct(Macro):
     
@@ -191,16 +85,12 @@ class mad_ct(Macro):
     def run(self, *args, **kwargs):
         time = args[0]
         oldMntGrp = self.getEnv("ActiveMntGrp")
-        var = "%s.ActiveMntGrp" % self.getDoorName()
-        self.setEnv(var, MNT_GRP)
-        self.execMacro("PrepareMaster")
-        self.execMacro("PrepareSlaves")
-        self.execMacro("PrepareGating")
+        self.setEnv("ActiveMntGrp", MNT_GRP)
+        self.execMacro("PrepareCountersForStepScanning")
         try:
             self.execMacro("ct", time)
         finally:
-            self.execMacro("CleanupGating")
-            self.setEnv(var, oldMntGrp)
+            self.setEnv("ActiveMntGrp", oldMntGrp)
 
 class mad_ascan(Macro):
     
@@ -218,13 +108,10 @@ class mad_ascan(Macro):
         Npts      = args[3]
         intTim    = args[4]      
         args = (mot.name,StartPos,EndPos,Npts,intTim)
-        mntGrp = self.getEnv("ActiveMntGrp")
-        self.execMacro("senv", "ActiveMntGrp", MNT_GRP)
-        self.execMacro("PrepareMaster")
-        self.execMacro("PrepareSlaves")
-        self.execMacro("PrepareGating")
+        oldMntGrp = self.getEnv("ActiveMntGrp")
+        self.setEnv("ActiveMntGrp", MNT_GRP)
+        self.execMacro("PrepareCountersForStepScanning")
         try:
             self.execMacro('ascan', *args)
         finally: 
-            self.execMacro('CleanupGating') 
             self.execMacro("senv", "ActiveMntGrp", mntGrp)
