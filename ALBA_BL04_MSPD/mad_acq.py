d2ab1bebbb3caf102f3971e8e9e82f9d23e31db5
droldanb <droldanb@users.sourceforge.net>
2013-06-19 15:12:01 +0000

    insert import macro_utils.mad26acq
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/macros/trunk@5531 a256e3e2-f7d5-44d2-9cb2-a5f5fb33e127

--- ALBA_BL04_MSPD/mad_acq.py
+++ ALBA_BL04_MSPD/mad_acq.py
@@ -1,82 +1,10 @@
 import time
 import PyTango, taurus
 from sardana.macroserver.macro import *
-
-COUNTERS = ["bl04/io/ibl0403-dev2-ctr0",
-            "bl04/io/ibl0403-dev2-ctr1",
-            "bl04/io/ibl0403-dev2-ctr2", 
-            "bl04/io/ibl0403-dev2-ctr3",
-            "bl04/io/ibl0403-dev2-ctr4",
-            "bl04/io/ibl0403-dev2-ctr5",
-            "bl04/io/ibl0403-dev2-ctr6",
-            "bl04/io/ibl0403-dev2-ctr7",
-            "bl04/io/ibl0403-dev3-ctr0",
-            "bl04/io/ibl0403-dev3-ctr1",
-            "bl04/io/ibl0403-dev3-ctr2",
-            "bl04/io/ibl0403-dev3-ctr3",
-            "bl04/io/ibl0403-dev3-ctr4",
-            "bl04/io/ibl0403-dev3-ctr5"]
-
-TRIGGER = "bl04/io/ibl0403-dev3-ctr6"
-TRIGGER_DEVICE = "bl04/io/ibl0403-dev3"
-TRIGGER_SOURCE = "/Dev3/PFI12"
-TRIGGER_DESTINATION = "/Dev3/RTSI0"
+from macro_utils.mad26acq import COUNTERS, PrepareCountersForStepScanning
 
 MNT_GRP = "mad"
 
-class PrepareGating(Macro):
-
-    param_def = []
-
-    def run(self, *args, **kwargs):
-        dev = taurus.Device(TRIGGER_DEVICE)
-        dev.ConnectTerms([TRIGGER_SOURCE, TRIGGER_DESTINATION, "DoNotInvertPolarity"])
-
-class CleanupGating(Macro):
-
-    param_def = []
-
-    def run(self, *args, **kwargs):
-        dev = taurus.Device(TRIGGER_DEVICE)
-        dev.DisconnectTerms([TRIGGER_SOURCE, TRIGGER_DESTINATION])
-
-class PrepareMaster(Macro):
-
-    param_def = []
-
-    def run(self, *args, **kwargs):
-        timerChannel = taurus.Device(TRIGGER)
-        if timerChannel.State() != PyTango.DevState.STANDBY:
-            timerChannel.Stop()
-        timerChannel.getAttribute("InitialDelayTime").write(0)
-        timerChannel.getAttribute("SampleMode").write("Finite")
-        timerChannel.getAttribute("SampleTimingType").write("Implicit")
-        timerChannel.getHWObj().write_attribute("SampPerChan", long(1))
-        timerChannel.getAttribute("IdleState").write("Low")        
-        timerChannel.getAttribute("LowTime").write(0.0000001)        
-
-class PrepareSlaves(Macro):
-    
-    param_def = []
-
-    def run(self, *args, **kwargs):
-        for channelName in COUNTERS:
-            channel = taurus.Device(channelName)
-            if channel.State() != PyTango.DevState.STANDBY:
-                channel.Stop()
-            channel.getAttribute("PauseTriggerType").write("DigLvl")
-            channel.getAttribute("PauseTriggerWhen").write("Low")
-            channel.getAttribute("PauseTriggerSource").write(TRIGGER_DESTINATION)
-
-class PrepareCountersForStepScanning(Macro):
-
-    param_def = []
-
-    def run(self, *args, **kwargs):
-        self.execMacro("PrepareMaster")
-        self.execMacro("PrepareSlaves")
-        self.execMacro("PrepareGating")
-        
 class InitCounters(Macro):
     
     param_def = []
@@ -94,7 +22,7 @@ class mad_ct(Macro):
         time = args[0]
         oldMntGrp = self.getEnv("ActiveMntGrp")
         self.setEnv("ActiveMntGrp", MNT_GRP)
-        self.execMacro("PrepareCountersForStepScanning")
+        PrepareCountersForStepScanning()
         try:
             self.execMacro("ct", time)
         finally:
@@ -118,7 +46,7 @@ class mad_ascan(Macro):
         args = (mot.name,StartPos,EndPos,Npts,intTim)
         oldMntGrp = self.getEnv("ActiveMntGrp")
         self.setEnv("ActiveMntGrp", MNT_GRP)
-        self.execMacro("PrepareCountersForStepScanning")
+        PrepareCountersForStepScanning()
         try:
             self.execMacro('ascan', *args)
         finally: 
