2a3686159ae019eb6cac87b64dd0dd7e92a82f1f
Teresa <tnunez@mail.desy.de>
2015-03-30 10:49:15 +0200

    Adding wg info

--- DESY_general/news.py
+++ DESY_general/news.py
@@ -7,6 +7,7 @@ class news(Macro):
     """   Outputs the list of most recent changes to Sardana
     """
     def run(self):
+        self.output( "30.03.2015 Macro wg added. It shows the position of the motors given as argument with the same format as wa")
         self.output( "11.03.2015 Counts attribute changed from Long to Double (SIS3820, VcExecutor, V260)")         
         self.output( "09.03.2015 added the 'news' macro")         
 
