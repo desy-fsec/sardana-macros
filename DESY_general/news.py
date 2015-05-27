2270ee46199f5966493ea147fd325448dd49f04a
Thorsten Kracht <thorsten.kracht@desy.de>
2015-05-27 12:21:05 +0200

    and another try

--- DESY_general/news.py
+++ DESY_general/news.py
@@ -7,7 +7,7 @@ class news(Macro):
     """   Outputs the list of most recent changes to Sardana
     """
     def run(self):
-        self.output( "27.05.2015 fixed mvsa.py to copy with aborted scans (creates an errror) ")
+        self.output( "27.05.2015 fixed mvsa.py to handle ctrl-c'd scans: creates an error")
         self.output( "30.03.2015 Macro wg added. It shows the position of the motors given as argument with the same format as wa")
         self.output( "11.03.2015 Counts attribute changed from Long to Double (SIS3820, VcExecutor, V260)")         
         self.output( "09.03.2015 added the 'news' macro")         
