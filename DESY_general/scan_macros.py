dd9e2baf5697e6329a257189dadd7feea05a1a66
teresa <tnunez@mail.desy.de>
2015-10-22 13:22:33 +0200

    Adding region_scans

--- DESY_general/scan_macros.py
+++ DESY_general/scan_macros.py
@@ -6,6 +6,7 @@ import time
 
 from sardana.macroserver.macro import *
 
+import math
 
 class lup(Macro):
     """Line-up scan:
@@ -74,3 +75,34 @@ class scan_loop(Macro):
             while 1:
                 self.execMacro('ascan', motor.getName(), start_pos, final_pos, nr_interv, integ_time)
 
+
+ 
+class region_scans(Macro):
+    """ Region scans """
+
+    param_def = [ 
+        ['motor',      Type.Moveable,   None, 'Motor to scan to move'],
+        ["scan_regions", ParamRepeat(
+                ['estart', Type.Float, None, 'Start position'],
+                ['estop', Type.Float, None, 'Stop position'],
+                ['estep', Type.Integer, None, 'Step size'],
+                ['integ_time', Type.Float, None, 'Integration time']),
+         None, 'List of scan regions']
+        ]
+    
+    def run(self, motor, *scan_regions):
+        
+        # calculate number of regions
+        nregions = len(scan_regions)
+        for i in range(0, nregions):
+            nenergies = int(math.fabs(scan_regions[i][1]-scan_regions[i][0])/scan_regions[i][2])     
+            if nenergies < 1:
+                nenergies = 1
+
+            macro,pars = self.createMacro('ascan', motor,
+                                          scan_regions[i][0],         # energy_start
+                                          scan_regions[i][1],         # energy_stop
+                                          nenergies,                  # number of steps
+                                          scan_regions[i][3])
+
+            self.runMacro(macro)
