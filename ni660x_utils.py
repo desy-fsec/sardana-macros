fc7c0ff29dd9129887d3623c22f5a937f8faf06d
Roberto J. Homs Puron <rhoms@cells.es>
2016-05-05 19:12:31 +0200

    Include connect macro
    Include ni_connect_channels macro to connect the internal signals.

--- ni660x_utils.py
+++ ni660x_utils.py
@@ -1,6 +1,16 @@
 
 from sardana.macroserver.macro import Macro, Type
-import PyTango
+import taurus
+
+
+NI660X_PFI = {'C0O': 'PFI36', 'C0A': 'PFI37', 'C0G': 'PFI38', 'C0S': 'PFI39',
+              'C1O': 'PFI32', 'C1A': 'PFI33', 'C1G': 'PFI34', 'C1S': 'PFI35',
+              'C2O': 'PFI28', 'C2A': 'PFI29', 'C2G': 'PFI30', 'C2S': 'PFI31',
+              'C3O': 'PFI24', 'C3A': 'PFI25', 'C3G': 'PFI26', 'C3S': 'PFI27',
+              'C4O': 'PFI20', 'C4A': 'PFI21', 'C4G': 'PFI22', 'C4S': 'PFI23',
+              'C5O': 'PFI16', 'C5A': 'PFI17', 'C5G': 'PFI18', 'C5S': 'PFI19',
+              'C6O': 'PFI12', 'C6A': 'PFI13', 'C6G': 'PFI14', 'C6S': 'PFI15',
+              'C7O': 'PFI08', 'C7A': 'PFI09', 'C7G': 'PFI10', 'C7S': 'PFI11'}
 
 
 class ni_trigger(Macro):
@@ -15,9 +25,9 @@ class ni_trigger(Macro):
 
     def run(self, ntriggers):
         ni_channel_name = self.getEnv('NITriggerChannel')
-        channel = PyTango.DeviceProxy(ni_channel_name)
+        channel = taurus.Device(ni_channel_name)
         channel.command_inout('Stop')
-        if ntriggers>0:
+        if ntriggers > 0:
             channel.write_attribute("SampPerChan", long(ntriggers))
         channel.command_inout('Start')
 
@@ -37,9 +47,8 @@ class ni_config_trigger(Macro):
 
     def run(self, high_time, low_time, ntriggers, idle_state, delay_time):
         ni_channel_name = self.getEnv('NITriggerChannel')
-        channel = PyTango.DeviceProxy(ni_channel_name)
+        channel = taurus.Device(ni_channel_name)
         channel.command_inout('Stop')
-        channel.command_inout('Init')
         channel.write_attribute("InitialDelayTime", delay_time)
         channel.write_attribute("HighTime", high_time)
         channel.write_attribute("LowTime", low_time)
@@ -47,3 +56,47 @@ class ni_config_trigger(Macro):
         channel.write_attribute("IdleState", idle_state)
         channel.write_attribute("SampleTimingType", "Implicit")
 
+
+class ni_connect_channels(Macro):
+    """
+    This macro connect the given channel signals on the NI6602. The channels 
+    are identified by: RTSI[0-7] and/or C[0-7][O,S,G,A] , where the letters 
+    are:
+        - O: Output
+        - S: Source
+        - G: Gate
+        - A: Auxiliary 
+    To introduce the channel you should include the card: Dev[1-n].
+
+    Note: 
+        - This macro use the enviromen variable NI660XDsName. 
+        - Sardana 2.0 API.
+    """
+
+    param_def = [['channels', [['channel', Type.String, None, '/Dev1/C10'],
+                               {'min': 2}],
+                  None, 'List of channels and internal signals']]
+
+    def run(self, channels):
+        connect_list = []
+        try:
+            ni_device_name = self.getEnv('NI660XDsName')
+        except Exception as e:
+            self.error('You should declare the Ni660XDsName. %s' % e)
+
+        ni_device = taurus.Device(ni_device_name)
+
+        for chn in channels:
+            chn = chn.upper()
+            if 'RTSI' not in chn:
+                # Change the channel name by its PFI
+                dev_chn = chn.rsplit('/', 1)
+                dev_chn[1] = NI660X_PFI[dev_chn[1]]
+                chn = '/'.join(dev_chn)
+            connect_list.append(chn)
+
+        # Include the last parameter DoNotInvertPolarity or InvertPolarity
+        for pair in connect_list[1:]:
+            cmd = [connect_list[0], pair, 'DoNotInvertPolarity']
+            self.debug(cmd)
+            ni_device.ConnectTerms(cmd)
