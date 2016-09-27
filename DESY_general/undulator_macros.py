11d6554ca54a7007ff1ba887a40dea3bb44d69b3
teresa <tnunez@mail.desy.de>
2016-09-27 13:58:04 +0200

    Macro setting harmonic

---
+++ DESY_general/undulator_macros.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""
+Macros related to petra3undulator
+"""
+
+__all__ = ["set_harmonic",
+	   ]
+
+import PyTango, os, sys
+import time
+from sardana.macroserver.macro import *
+from sardana.macroserver.macro import macro
+
+
+class set_harmonic(Macro):
+    """set new harmonic value"""
+    
+    param_def = [
+        ['harmonic', Type.Integer, None, 'Harmonic value to set'],
+        ]
+    
+    def run(self, harmonic):
+        undulator_device_name = "energy_und"
+        try:
+            undulator_device_name = self.getEnv('UndulatorDevice')
+        except:
+            pass
+                    
+        try:
+            undulator_device = PyTango.DeviceProxy(undulator_device_name)
+        except:
+            self.error("Not able to create proxy to undulator device %s." % undulator_device_name)
+            self.info("\nSet the name of the Pool undulator device in the Spock environment variable UndulatorDevice, ex.:\n\n   senv UndulatorDevice undulator")
+            return
+
+        undulator_tango_device = PyTango.DeviceProxy(undulator_device.TangoDevice)
+
+        try:
+            undulator_tango_device.write_attribute("Harmonic", harmonic)
+        except:
+            self.error("Unable to set harmonic in Tango Undulator device %s" % undulator_device.TangoDevice)
+            return
+        
+        try:
+            current_position = undulator_tango_device.read_attribute("Position").value
+        except:
+            self.error("Unable to read current undulator position")
+            return
+        
+        try:
+            undulator_device.write_attribute("Position", current_position)
+        except:
+            self.error("Unable to set Position to undulator device")
+            return
