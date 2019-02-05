cfeca679b7ef8a8df1ccc4c910b8b63ff7920f27
Roberto Javier Homs Puron <rhoms@cells.es>
2019-02-05 17:04:13 +0100

    Fix autorange procedure macro.

--- ALBA_GENERAL/albaEmUtils.py
+++ ALBA_GENERAL/albaEmUtils.py
@@ -177,15 +177,16 @@ class em_range(Macro):
                   [['ch', Type.CTExpChannel, None, 'electrometer chn'],
                    ['range',  Type.String, None, 'Amplifier range'],
                    {'min': 1}],
-                  None, 'List of [channels,range]'],]
-
+                  None, 'List of [channels,range]']]
+    enabled_output = True
     def run(self, chns):
         for ch, rg in chns:
             old_range = ch.read_attribute("Range").value
             ch.write_attribute("Range", rg)
             new_range = ch.read_attribute("Range").value
-            self.output('%s changed range from %s to %s' % (ch, old_range,
-                                                            new_range))
+            if self.enabled_output:
+                self.output('%s changed range from %s to %s' % (ch, old_range,
+                                                                new_range))
 
 
 class em_inversion(Macro):
@@ -215,7 +216,7 @@ class em_autorange(Macro):
                   [['ch', Type.CTExpChannel, None, 'electrometer chn'],
                    ['enabled', Type.Boolean, None, 'Inversion enabled'],
                    {'min': 1}],
-                  None, 'List of [channels, inversion]'], ]
+                  None, 'List of [channels, inversion]'] ]
 
     def run(self, chns):
         for ch, enabled in chns:
@@ -267,18 +268,21 @@ class em_findmaxrange(Macro):
                  ['wait_time', Type.Float, 3, 'time to applied the autorange'],
                  ]
 
-    RANGES = ['1mA', '100uA', '10uA', '1uA', '100nA', '10nA', '1nA', '100pA']
+    RANGES = ['1mA', '100uA', '10uA', '1uA', '100nA', '10nA', '1nA',
+              '100pA', 'none']
 
     def run(self, motor, positions, chns, wait_time):
         chns_ranges = {}
+        previous_chns_ranges = {}
 
         for chn in chns:
-            range = chn.range
-            chns_ranges[chn] = range
-            self.debug('{0}: {1}'.format(chn.name, range))
+            chns_ranges[chn] = 'none'
+            previous_chns_ranges[chn] = chn.range
+            self.debug('{0}: {1}'.format(chn.name, 'none'))
 
         for energy in positions:
             self.umv(motor, energy)
+
             self.em_findrange(chns, wait_time)
             for chn, prev_range in chns_ranges.items():
                 new_range = chn.range
@@ -289,7 +293,14 @@ class em_findmaxrange(Macro):
 
         self.info('Setting maximum range...')
         chns_cfg = []
-        for chn, range in chns_ranges.items():
-            chns_cfg.append([chn, range])
-        self.em_range(chns_cfg)
+        for chn, new_range in chns_ranges.items():
+            chns_cfg.append([chn, new_range])
+        em_range, _ = self.createMacro('em_range', chns_cfg)
+        em_range.enabled_output = False
+        self.runMacro(em_range)
+        for chn, new_range in chns_ranges.items():
+            prev_range = previous_chns_ranges[chn]
+            self.output('{0} changed range from {1} ' 
+                        'to {2}'.format(chn, prev_range, new_range))
+
 
