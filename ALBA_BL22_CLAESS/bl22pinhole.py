b52853c78f1b10ca5839c84f94f87e045238c50e
Roberto Javier Homs Puron <rhoms@cells.es>
2019-05-21 15:10:16 +0200

    Add pin-hole macros to save/restore the alignment position.

--- ALBA_BL22_CLAESS/bl22pinhole.py
+++ ALBA_BL22_CLAESS/bl22pinhole.py
@@ -30,3 +30,41 @@ class lim_tp(Macro):
             if pseudo_min < t_min:
                 pseudo_min = t_min
             self.set_lim(pseudo, pseudo_min, pseudo_max)
+
+
+class saveph(Macro):
+    """
+    Macro to save the current position of the motors (phx, phz, tripod_z,
+    tripod_x) on the environment variables phx_pos, phz_pos, tripod_x_pos,
+    tripod_z_pos of the macro restore_ph
+    """
+
+    def run(self):
+        motors_names = ['phx', 'phz', 'tripod_x', 'tripod_z']
+
+        for motor_name in motors_names:
+            motor = self.getMoveable(motor_name)
+            pos = motor.position
+            env_name = 'restoreph.{}_pos'.format(motor_name)
+            self.setEnv(env_name, pos)
+        self.lim_tp()
+
+
+class restoreph(Macro):
+    """
+    Macro to restore the motors (phx, phz, tripod_z, tripod_x) from the
+    environment variables phx_pos, phz_pos, tripod_x_pos,
+    tripod_z_pos
+    """
+
+    def run(self):
+        motors_names = ['phx', 'phz', 'tripod_x', 'tripod_z']
+        cmd = 'mv '
+
+        for motor_name in motors_names:
+            env_name = '{}_pos'.format(motor_name)
+            pos = self.getEnv(env_name)
+            cmd += '{} {} '.format(motor_name, pos)
+        self.output(cmd)
+        self.execMacro(cmd)
+
