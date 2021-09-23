f8f16459e0487b59227fc2b33f431775f3e0d1a4
Jan Kotanski <jankotan@gmail.com>
2021-09-23 12:41:36 +0200

    change #!/bin/env python to #!/usr/bin/env python

--- DESY_general/PiLCContinuousScans.py
+++ DESY_general/PiLCContinuousScans.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 """pilc triggered continuous scan macros"""
 
--- DESY_general/nxsmacros.py
+++ DESY_general/nxsmacros.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 """ NeXus recorder macros """
 
 import PyTango
